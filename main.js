afd8a0ddf101851840c784dff51dc3661d293b5a
hahyeon <hh_ch0@naver.com>
2024-06-18 16:59:37 +0900

    undo redo 버튼 추가 + redo 버그

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const lineWidth = document.querySelector("#line-width");
 const colorBox = document.querySelectorAll(".color-box");
 const screenClearBtn = document.querySelector("#screen-clear-btn");
+const undoBtn = document.querySelector("#undo-btn");
 const redoBtn = document.querySelector("#redo-btn");
 const memoryClearBtn = document.querySelector("#memory-clear-btn");
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
-const data = [];
+const colorset = ["black", "pink", "red", "green", "blue"];
+const colorButtons = document.querySelectorAll(".color-box");
+colorButtons.forEach((button, index) => {
+  button.style.backgroundColor = colorset[index];
+});
 
 let isDrawing = false;
 
@@ -35,7 +40,7 @@ const drawFalse = () => {
 };
 
 const changeColor = (e) => {
-  ctx.strokeStyle = e.target.id;
+  ctx.strokeStyle = e.target.style.backgroundColor;
 };
 
 const changeLineWidth = (e) => {
@@ -46,7 +51,74 @@ const screenClear = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
 
-canvas.addEventListener("mousedown", drawTrue);
+////////////////////////////////////////////////
+
+// 1. canvas에 그릴 때마다 path를 배열로 저장 (stack)
+// --> 어떻게????? 어떤 타입? 뭘로? ... ??????
+// 2. 배열에 있는 걸 하나씩 pop
+// --> X!!! pop 하면 넣어놓은 path가 삭제돼서 redo가 안됨!!!
+// 3. 나타내기? 그리기? stroke?
+
+let data = [];
+let step = -1;
+
+const push = () => {
+  // canvas의 그림을 data에 저장 -> canvas.??
+  // canvas.toDataURL() : 매개변수 에 지정된 형식의 이미지 표현이 포함된 데이터 URL을 type 반환합니다.
+  // data.push(canvas);
+  // console.log(data);
+  step++;
+  data.push(canvas.toDataURL());
+  // console.log(data);
+  console.log("step", step);
+  console.log("data.length", data.length);
+};
+
+const memoryClear = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  data = [];
+  step = -1;
+};
+
+undoBtn.addEventListener("click", () => {
+  if (data.length === 0) return;
+  let newCanvasImage = new Image();
+  // console.log(data.pop());
+  // newCanvasImage.src = data.pop();
+  // console.log(data[step]);
+  newCanvasImage.src = data[step];
+  newCanvasImage.onload = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(newCanvasImage, 0, 0);
+  };
+  if (step > -1) {
+    step--;
+    console.log("step", step);
+  }
+});
+
+redoBtn.addEventListener("click", () => {
+  if (data.length < step) return;
+
+  if (step < data.length - 1) {
+    step++;
+    console.log("step", step);
+    let newCanvasImage = new Image();
+    newCanvasImage.src = data[step];
+    newCanvasImage.onload = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(newCanvasImage, 0, 0);
+    };
+  }
+});
+
+///////////////////////////////////////////////
+
+canvas.addEventListener("mousedown", () => {
+  drawTrue();
+  // path 하나 그릴 때마다 push
+  push();
+});
 canvas.addEventListener("mouseup", drawFalse);
 canvas.addEventListener("mouseout", drawFalse);
 canvas.addEventListener("mousemove", draw);
@@ -55,4 +127,4 @@ colorBox.forEach((color) => {
   color.addEventListener("click", changeColor);
 });
 screenClearBtn.addEventListener("click", screenClear);
-redoBtn.addEventListener("click", () => {});
+memoryClearBtn.addEventListener("click", memoryClear);
